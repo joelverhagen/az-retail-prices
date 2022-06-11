daf16d81a9a27ae2b70daa12a310b7aa80df4a1c
Joel Verhagen <joel.verhagen@gmail.com>
2022-06-11 16:19:20 -0700

    Optimize and split the SQLite DB

--- pages/postprocess.sh
+++ pages/postprocess.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-SCRIPT_DIR="$(dirname "$0")"
+SCRIPT_DIR=$(dirname "$0")
+OUT_DIR=$SCRIPT_DIR/build/data
+DB_URL=https://github.com/joelverhagen/data-az-retail-prices/releases/download/latest/azure-prices.db
+DB_PATH=$SCRIPT_DIR/build/data/azure-prices.db
+
+# Set up the data directory
+rm -rf "$OUT_DIR"
 mkdir "$SCRIPT_DIR/build/data" --parents
 
-DB_URL=https://github.com/joelverhagen/data-az-retail-prices/releases/download/latest/azure-prices.db
-wget "$DB_URL" -O "$SCRIPT_DIR/build/data/azure-prices.db"
+# Download the latest DB
+wget "$DB_URL" -O "$DB_PATH" -nv
+
+# Optimize the DB
+# Steps from https://github.com/phiresky/sql.js-httpvfs
+sqlite3 "$DB_PATH" "pragma journal_mode = delete"
+sqlite3 "$DB_PATH" "pragma page_size = 1024"
+sqlite3 "$DB_PATH" "vacuum"
+
+# Split the DB
+/bin/bash "$SCRIPT_DIR/create_db.sh" "$DB_PATH" "$OUT_DIR"
+rm "$DB_PATH"
